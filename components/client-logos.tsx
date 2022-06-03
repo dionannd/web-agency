2bbd6ade642e1fedc104b2cabfc18127a5067025
dionannd <dionananda77@gmail.com>
2022-06-03 17:49:18 +0700

    fix(client-logos): fix margin

--- components/client-logos.tsx
+++ components/client-logos.tsx
@@ -4,13 +4,13 @@ import SliderContainer, { SliderItem } from "./slider";
 
 const ClientLogos: React.FC = () => (
   <>
-    <SliderContainer className="" contentWidth={1290} initialOffsetX={0}>
+    <SliderContainer className="mb-4" contentWidth={1290} initialOffsetX={0}>
       <SliderItem width={150}>
         <Image 
         src="/assets/trustedby/coinbase.webp"
         width={150}
         height={50}
-        alt="Audubon"
+        alt="Coinbase"
         objectFit="contain"
       />  
       </SliderItem>
@@ -77,8 +77,188 @@ const ClientLogos: React.FC = () => (
         objectFit="contain"
       />  
      </SliderItem>
+     <SliderItem width={150}>
+        <Image 
+        src="/assets/trustedby/shopify.webp"
+        width={150}
+        height={50}
+        alt="Audubon"
+        objectFit="contain"
+      />  
+     </SliderItem>
+      <SliderItem width={150}>
+        <Image 
+        src="/assets/trustedby/coinbase.webp"
+        width={150}
+        height={50}
+        alt="Coinbase"
+        objectFit="contain"
+      />  
+      </SliderItem>
+      <SliderItem width={150}>
+        <Image 
+        src="/assets/trustedby/rainbow.webp"
+        width={150}
+        height={50}
+        alt="Audubon"
+        objectFit="contain"
+      />  
+      </SliderItem>
+      <SliderItem width={150}>
+        <Image 
+        src="/assets/trustedby/walletconnect.webp"
+        width={150}
+        height={50}
+        alt="Audubon"
+        objectFit="contain"
+      />  
+     </SliderItem>
+     <SliderItem width={150}>
+        <Image 
+        src="/assets/trustedby/exodus.webp"
+        width={150}
+        height={50}
+        alt="Audubon"
+        objectFit="contain"
+      />  
+     </SliderItem>
+     <SliderItem width={150}>
+        <Image 
+        src="/assets/trustedby/picnic.webp"
+        width={150}
+        height={50}
+        alt="Audubon"
+        objectFit="contain"
+      />  
+     </SliderItem>
+     <SliderItem width={150}>
+        <Image 
+        src="/assets/trustedby/status.webp"
+        width={150}
+        height={50}
+        alt="Audubon"
+        objectFit="contain"
+      />  
+     </SliderItem>
+     <SliderItem width={150}>
+        <Image 
+        src="/assets/trustedby/extra.webp"
+        width={150}
+        height={50}
+        alt="Audubon"
+        objectFit="contain"
+      />  
+     </SliderItem>
+     <SliderItem width={150}>
+        <Image 
+        src="/assets/trustedby/stori.webp"
+        width={150}
+        height={50}
+        alt="Audubon"
+        objectFit="contain"
+      />  
+     </SliderItem>
+     <SliderItem width={150}>
+        <Image 
+        src="/assets/trustedby/shopify.webp"
+        width={150}
+        height={50}
+        alt="Audubon"
+        objectFit="contain"
+      />  
+      </SliderItem>
     </SliderContainer>
     <SliderContainer className="" contentWidth={1290} initialOffsetX={0}>
+      <SliderItem width={150}>
+        <Image 
+          src="/assets/trustedby/scribeware.webp"
+          width={150}
+          height={50}
+          alt="Audubon"
+          objectFit="contain"
+        />
+      </SliderItem>
+      <SliderItem width={150}>
+        <Image 
+          src="/assets/trustedby/snapcalorie.webp"
+          width={150}
+          height={50}
+          alt="Audubon"
+          objectFit="contain"
+        />
+      </SliderItem>
+      <SliderItem width={150}>
+        <Image 
+          src="/assets/trustedby/tocsen.webp"
+          width={150}
+          height={50}
+          alt="Audubon"
+          objectFit="contain"
+        />
+      </SliderItem>
+      <SliderItem width={150}>
+        <Image 
+          src="/assets/trustedby/audubon.webp"
+          width={150}
+          height={50}
+          alt="Audubon"
+          objectFit="contain"
+        />
+      </SliderItem>
+      <SliderItem width={150}>
+        <Image 
+          src="/assets/trustedby/slingshot.webp"
+          width={150}
+          height={50}
+          alt="Audubon"
+          objectFit="contain"
+        />
+      </SliderItem>
+      <SliderItem width={150}>
+        <Image 
+          src="/assets/trustedby/showtime.webp"
+          width={150}
+          height={50}
+          alt="Audubon"
+          objectFit="contain"
+        />
+      </SliderItem>
+      <SliderItem width={150}>
+        <Image 
+          src="/assets/trustedby/steakwallet.webp"
+          width={150}
+          height={50}
+          alt="Audubon"
+          objectFit="contain"
+        />
+      </SliderItem>
+      <SliderItem width={150}>
+        <Image 
+          src="/assets/trustedby/expensify.webp"
+          width={150}
+          height={50}
+          alt="Audubon"
+          objectFit="contain"
+        />
+      </SliderItem>
+      <SliderItem width={150}>
+        <Image 
+          src="/assets/trustedby/pinkpanda.webp"
+          width={150}
+          height={50}
+          alt="Audubon"
+          objectFit="contain"
+        />
+      </SliderItem>
+       <SliderItem width={150}>
+        <Image 
+          src="/assets/trustedby/litentry.webp"
+          width={150}
+          height={50}
+          alt="Audubon"
+          objectFit="contain"
+        />
+      </SliderItem>
       <SliderItem width={150}>
         <Image 
           src="/assets/trustedby/scribeware.webp"
